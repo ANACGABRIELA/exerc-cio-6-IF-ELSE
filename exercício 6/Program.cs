0f8f440f30d14f4c53082d22f333606125bd061f
ana.silva69 <ana.silva69@ITELABD10>
2022-05-19 21:06:50 -0300

    Adicionar arquivos de projeto.

---
+++ exercício 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercício_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ano;
+
+
+            Console.WriteLine("escolha um ano");
+            ano = Convert.ToDouble(Console.ReadLine());
+
+            if (ano % 4 == 0 && ano % 100 != 0)
+            {
+               
+                Console.WriteLine("O ano " + ano + " e bissexto.");
+            }
+            else
+            {
+                Console.WriteLine("O ano " + ano + " nao e Bissexto.");
+            }
+        }
+    }
+}
